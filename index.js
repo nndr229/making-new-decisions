70de305f33f63627bf2a8b38a33f9d09d550167f
Nithin Deepkumar <nithindeepkumar@Nithins-MacBook-Pro.local>
2025-01-26 13:58:38 +0530

    intial commit Making new Decisons

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Set EJS as the view engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// Serve static files (CSS, images, etc.) from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Home route
+app.get('/', (req, res) => {
+  res.render('home');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
